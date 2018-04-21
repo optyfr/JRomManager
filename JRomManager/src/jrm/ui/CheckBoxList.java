f0781230e90f704077fe1a98730c9f33d6308547
optyfr <17027109+optyfr@users.noreply.github.com>
2018-04-21 15:05:25 +0200

    CheckBoxList

---
+++ JRomManager/src/jrm/ui/CheckBoxList.java
@@ -0,0 +1,57 @@
+package jrm.ui;
+
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JCheckBox;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListSelectionModel;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class CheckBoxList<E> extends JList<E>
+{
+	protected static Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
+
+	public CheckBoxList()
+	{
+		setCellRenderer(new CellRenderer());
+
+		addMouseListener(new MouseAdapter()
+		{
+			public void mousePressed(MouseEvent e)
+			{
+				int index = locationToIndex(e.getPoint());
+
+				if(index != -1)
+				{
+					JCheckBox checkbox = (JCheckBox) getModel().getElementAt(index);
+					checkbox.setSelected(!checkbox.isSelected());
+					repaint();
+				}
+			}
+		});
+
+		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	}
+
+	protected class CellRenderer implements ListCellRenderer<E>
+	{
+		public Component getListCellRendererComponent(JList<? extends E> list, E value, int index, boolean isSelected, boolean cellHasFocus)
+		{
+			JCheckBox checkbox = (JCheckBox) value;
+			checkbox.setBackground(isSelected ? getSelectionBackground() : getBackground());
+			checkbox.setForeground(isSelected ? getSelectionForeground() : getForeground());
+			checkbox.setEnabled(isEnabled());
+			checkbox.setFont(getFont());
+			checkbox.setFocusPainted(false);
+			checkbox.setBorderPainted(true);
+			checkbox.setBorder(isSelected ? UIManager.getBorder("List.focusCellHighlightBorder") : noFocusBorder);
+			return checkbox;
+		}
+	}
+}
