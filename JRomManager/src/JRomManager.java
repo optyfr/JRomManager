801a5c5acd3083302295bfd8e04147a27595ec24
optyfr <17027109+optyfr@users.noreply.github.com>
2018-04-21 13:12:36 +0200

    Prevent launching multiple instances

--- JRomManager/src/JRomManager.java
+++ JRomManager/src/JRomManager.java
@@ -1,11 +1,52 @@
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
+
+import org.apache.commons.io.FilenameUtils;
+
+import jrm.misc.Log;
 import jrm.ui.MainFrame;
 
 public final class JRomManager
 {
 	public static void main(String[] args)
 	{
-		MainFrame window = new MainFrame();
-		window.setVisible(true);
+		if(lockInstance(FilenameUtils.removeExtension(JRomManager.class.getSimpleName()) + ".lock"))
+			new MainFrame().setVisible(true);
 	}
 
+	private static boolean lockInstance(final String lockFile)
+	{
+		try
+		{
+			final File file = new File(lockFile);
+			final RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+			final FileLock fileLock = randomAccessFile.getChannel().tryLock();
+			if(fileLock != null)
+			{
+				Runtime.getRuntime().addShutdownHook(new Thread()
+				{
+					public void run()
+					{
+						try
+						{
+							fileLock.release();
+							randomAccessFile.close();
+							file.delete();
+						}
+						catch(Exception e)
+						{
+							Log.err("Unable to remove lock file: " + lockFile, e);
+						}
+					}
+				});
+				return true;
+			}
+		}
+		catch(Exception e)
+		{
+			Log.err("Unable to create and/or lock file: " + lockFile, e);
+		}
+		return false;
+	}
 }
