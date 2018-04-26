418083c6f91d1c14385e4c8d69264d8f2173863c
optyfr <17027109+optyfr@users.noreply.github.com>
2018-04-26 15:17:02 +0200

    Added garbage filter when importing xml from mame executable

--- JRomManager/src/jrm/profile/Import.java
+++ JRomManager/src/jrm/profile/Import.java
@@ -1,7 +1,7 @@
 package jrm.profile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.Paths;
 
 import org.apache.commons.io.FilenameUtils;
@@ -24,12 +24,25 @@ public class Import
 		String ext = FilenameUtils.getExtension(file.getName());
 		if(ext.equalsIgnoreCase("exe")) //$NON-NLS-1$
 		{
-		//	Log.info("Get dat file from Mame...");
+			// Log.info("Get dat file from Mame...");
 			try
 			{
 				File tmpfile = File.createTempFile("JRM", ".xml"); //$NON-NLS-1$ //$NON-NLS-2$
 				tmpfile.deleteOnExit();
-				Process process = new ProcessBuilder(file.getAbsolutePath(), sl?"-listsoftware":"-listxml").directory(file.getAbsoluteFile().getParentFile()).redirectOutput(tmpfile).start(); //$NON-NLS-1$
+				Process process = new ProcessBuilder(file.getAbsolutePath(), sl ? "-listsoftware" : "-listxml").directory(file.getAbsoluteFile().getParentFile()).start(); //$NON-NLS-1$
+
+				try(BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(tmpfile), Charset.forName("UTF-8"))); BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("UTF-8")));)
+				{
+					String line;
+					boolean xml = false;
+					while(null != (line = in.readLine()))
+					{
+						if(line.startsWith("<?xml"))
+							xml = true;
+						if(xml)
+							out.write(line + "\n");
+					}
+				}
 				process.waitFor();
 				this.is_mame = true;
 				this.file = tmpfile;
