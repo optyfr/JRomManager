1b7494ef4dab060e624ad5818a1b1d47587ecc0d
optyfr <17027109+optyfr@users.noreply.github.com>
2018-05-02 18:02:19 +0200

    Using integrated Jtrrntzip

--- JRomManager/src/jrm/profile/fix/actions/TZipContainer.java
+++ JRomManager/src/jrm/profile/fix/actions/TZipContainer.java
@@ -8,6 +8,9 @@ import java.io.InputStreamReader;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.text.StringEscapeUtils;
 
+import JTrrntzip.LogCallback;
+import JTrrntzip.TorrentZip;
+import JTrrntzip.TorrentZipOptions;
 import jrm.misc.FindCmd;
 import jrm.misc.Settings;
 import jrm.profile.data.Container;
@@ -36,7 +39,40 @@ public class TZipContainer extends ContainerAction
 				{
 					if(container.file.exists())
 					{
-				//		System.out.println(container.file);
+						new TorrentZip(new LogCallback()
+						{
+							@Override
+							public void StatusCallBack(int percent)
+							{
+							}
+							
+							@Override
+							public boolean isVerboseLogging()
+							{
+								return false;
+							}
+							
+							@Override
+							public void StatusLogCallBack(String log)
+							{
+							}
+						}, new TorrentZipOptions()
+						{
+							
+							@Override
+							public boolean isForceRezip()
+							{
+								return false;
+							}
+							
+							@Override
+							public boolean isCheckOnly()
+							{
+								return false;
+							}
+						}).Process(container.file);
+/*						
+						
 						ProcessBuilder pb = new ProcessBuilder(tzip_cmd.getPath(), container.file.getAbsolutePath()).directory(tzip_cmd.getParentFile()).redirectErrorStream(true);
 						Process process = pb.start();
 						try(BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream())))
@@ -50,11 +86,11 @@ public class TZipContainer extends ContainerAction
 							for(File log : tzip_cmd.getParentFile().getParentFile().listFiles((dir, name) -> FilenameUtils.getExtension(name).equals("log")))
 								log.delete();
 						}
-						return process.waitFor() == 0;
+						return process.waitFor() == 0;*/
 					}
 					return true;
 				}
-				catch(InterruptedException | IOException e)
+				catch(/*InterruptedException |*/ IOException e)
 				{
 					e.printStackTrace();
 				}
