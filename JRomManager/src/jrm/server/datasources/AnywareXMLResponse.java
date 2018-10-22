35fc84ba6211ef4691bd959214c903045fe806dd
optyfr <17027109+optyfr@users.noreply.github.com>
2018-10-22 22:08:09 +0200

    ProfileViewer (webclient)

--- JRomManager/src/jrm/server/datasources/AnywareXMLResponse.java
+++ JRomManager/src/jrm/server/datasources/AnywareXMLResponse.java
@@ -4,8 +4,11 @@ import javax.xml.stream.XMLStreamException;
 
 import jrm.profile.data.Anyware;
 import jrm.profile.data.AnywareList;
-import jrm.profile.data.Entity;
+import jrm.profile.data.Disk;
+import jrm.profile.data.EntityBase;
 import jrm.profile.data.MachineList;
+import jrm.profile.data.Rom;
+import jrm.profile.data.Sample;
 import jrm.server.datasources.XMLRequest.Operation;
 
 public class AnywareXMLResponse extends XMLResponse
@@ -42,7 +45,7 @@ public class AnywareXMLResponse extends XMLResponse
 		return aw;
 	}
 	
-	private void write_record(final AnywareList<?> al, final Anyware aw, final Entity e)
+	private void write_record(final AnywareList<?> al, final Anyware aw, final EntityBase e)
 	{
 		try
 		{
@@ -50,6 +53,45 @@ public class AnywareXMLResponse extends XMLResponse
 			writer.writeAttribute("list", al instanceof MachineList?"*":al.getBaseName());
 			writer.writeAttribute("ware", aw.getBaseName());
 			writer.writeAttribute("name", e.getBaseName());
+			writer.writeAttribute("status", e.getStatus().toString());
+			if (e instanceof Rom)
+			{
+				Rom r = (Rom)e;
+				writer.writeAttribute("type", "ROM");
+				if(r.getSize()>0)
+					writer.writeAttribute("size", Long.toString(r.getSize()));
+				if(r.getCRC()!=null)
+					writer.writeAttribute("crc", r.getCRC());
+				if(r.getMD5()!=null)
+					writer.writeAttribute("md5", r.getMD5());
+				if(r.getSHA1()!=null)
+					writer.writeAttribute("sha1", r.getSHA1());
+				if(r.merge!=null)
+					writer.writeAttribute("merge", r.merge);
+				if(r.status!=null)
+					writer.writeAttribute("dumpstatus", r.status.toString());
+			}
+			else if (e instanceof Disk)
+			{
+				Disk d = (Disk)e;
+				writer.writeAttribute("type", "DISK");
+				if(d.getSize()>0)
+					writer.writeAttribute("size", Long.toString(d.getSize()));
+				if(d.getCRC()!=null)
+					writer.writeAttribute("crc", d.getCRC());
+				if(d.getMD5()!=null)
+					writer.writeAttribute("md5", d.getMD5());
+				if(d.getSHA1()!=null)
+					writer.writeAttribute("sha1", d.getSHA1());
+				if(d.merge!=null)
+					writer.writeAttribute("merge", d.merge);
+				if(d.status!=null)
+					writer.writeAttribute("dumpstatus", d.status.toString());
+			}
+			else if (e instanceof Sample)
+			{
+				writer.writeAttribute("type", "SAMPLE");
+			}
 		}
 		catch (XMLStreamException ex)
 		{
@@ -70,7 +112,7 @@ public class AnywareXMLResponse extends XMLResponse
 			if(reset)
 				aw.reset();
 			fetch_array(operation, aw.getRowCount(), (i, count) -> {
-				write_record(al, aw, (Entity)aw.getValueAt(i, 0));
+				write_record(al, aw, (EntityBase)aw.getValueAt(i, 0));
 			});
 		}
 		writer.writeEndElement();
