8fa074a60eda76ebe1d5a3cd441f4cb2e27b5d61
optyfr <17027109+optyfr@users.noreply.github.com>
2021-05-22 11:31:29 +0200

    code quality

--- JRomManager/src/jrm/profile/manager/ProfileNFO.java
+++ JRomManager/src/jrm/profile/manager/ProfileNFO.java
@@ -1,18 +1,19 @@
-/* Copyright (C) 2018  optyfr
+/*
+ * Copyright (C) 2018 optyfr
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package jrm.profile.manager;
 
@@ -21,7 +22,6 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -33,20 +33,16 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder;
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.io.FilenameUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -57,6 +53,7 @@ import jrm.security.Session;
 
 /**
  * The Profile NFO file managing class with tolerant manual (de)serialization
+ * 
  * @author optyfr
  */
 public final class ProfileNFO implements Serializable, HTMLRenderer
@@ -82,22 +79,31 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 
 	/**
 	 * fields declaration for manual serialization
-	 * @serialField file File the file linked to this profile info
-	 * @serialField name String the name of the profile
-	 * @serialField stats ProfileNFOStats the stats related to the profile
-	 * @serialField mame ProfileNFOMame the mame infos relates to the profile
+	 * 
+	 * @serialField file
+	 *                  File the file linked to this profile info
+	 * @serialField name
+	 *                  String the name of the profile
+	 * @serialField stats
+	 *                  ProfileNFOStats the stats related to the profile
+	 * @serialField mame
+	 *                  ProfileNFOMame the mame infos relates to the profile
 	 */
 	private static final ObjectStreamField[] serialPersistentFields = { new ObjectStreamField("file", File.class), new ObjectStreamField("name", String.class), new ObjectStreamField("stats", ProfileNFOStats.class), new ObjectStreamField("mame", ProfileNFOMame.class), }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 
 	/**
 	 * Manually write serialization
-	 * @serialData Use {@link ObjectOutputStream.PutField} object to add to persistent fields buffer, then {@link ObjectOutputStream#writeFields()}
-	 * @param stream the destination {@link ObjectOutputStream}
+	 * 
+	 * @serialData Use {@link ObjectOutputStream.PutField} object to add to
+	 *             persistent fields buffer, then
+	 *             {@link ObjectOutputStream#writeFields()}
+	 * @param stream
+	 *            the destination {@link ObjectOutputStream}
 	 * @throws IOException
 	 */
 	private void writeObject(final java.io.ObjectOutputStream stream) throws IOException
 	{
-		final ObjectOutputStream.PutField fields = stream.putFields();
+		final var fields = stream.putFields();
 		fields.put("file", file); //$NON-NLS-1$
 		fields.put("name", name); //$NON-NLS-1$
 		fields.put("stats", stats); //$NON-NLS-1$
@@ -107,7 +113,9 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 
 	/**
 	 * Manually read serialization
-	 * @param stream the destination {@link ObjectInputStream}
+	 * 
+	 * @param stream
+	 *            the destination {@link ObjectInputStream}
 	 * @throws IOException
 	 * @throws ClassNotFoundException
 	 */
@@ -122,20 +130,24 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 
 	/**
 	 * internal constructor
-	 * @param file the file to attach
+	 * 
+	 * @param file
+	 *            the file to attach
 	 */
 	private ProfileNFO(final File file)
 	{
 		this.file = file;
 		name = file.getName();
 		stats.created = new Date();
-		if(isJRM())
+		if (isJRM())
 			loadJrm(file);
 	}
 
 	/**
 	 * return the nfo file derived from the attached file
-	 * @param file the attached file candidate
+	 * 
+	 * @param file
+	 *            the attached file candidate
 	 * @return the nfo {@link File}
 	 */
 	private static File getFileNfo(final Session session, final File file)
@@ -145,7 +157,9 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 
 	/**
 	 * Delete NFO old location, change attached file, then save to new location
-	 * @param file new file to attach
+	 * 
+	 * @param file
+	 *            new file to attach
 	 */
 	public void relocate(final Session session, final File file)
 	{
@@ -157,23 +171,25 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 
 	/**
 	 * Load or create a ProfileNFO from an attached file
-	 * @param file the attached file from which to derive NFO file
+	 * 
+	 * @param file
+	 *            the attached file from which to derive NFO file
 	 * @return the {@link ProfileNFO}
 	 */
 	public static ProfileNFO load(final Session session, final File file)
 	{
-		final File filenfo = ProfileNFO.getFileNfo(session, file);
-		if(filenfo.lastModified() >= file.lastModified()) // $NON-NLS-1$
+		final var filenfo = ProfileNFO.getFileNfo(session, file);
+		if (filenfo.lastModified() >= file.lastModified()) // $NON-NLS-1$
 		{
-			try(ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(filenfo))))
+			try (final var ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(filenfo))))
 			{
 				ProfileNFO nfo = (ProfileNFO) ois.readObject();
-				if(nfo.file!=null)
+				if (nfo.file != null)
 					return nfo;
 			}
-			catch(final Throwable e)
+			catch (final Exception e)
 			{
-			//	Log.err(e.getMessage(),e);
+				// Log.err(e.getMessage(),e);
 			}
 		}
 		return new ProfileNFO(file);
@@ -184,29 +200,31 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 	 */
 	public void save(final Session session)
 	{
-		if(isJRM()) try
-		{
-			long modified = file.lastModified();
-			saveJrm(file, mame.fileroms, mame.filesl);
-			if(modified!=0)
-				file.setLastModified(modified);
-		}
-		catch (ParserConfigurationException | TransformerException e)
-		{
-			Log.err(e.getMessage(),e);
-		}
-		try(ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(ProfileNFO.getFileNfo(session, file)))))
+		if (isJRM())
+			try
+			{
+				long modified = file.lastModified();
+				saveJrm(file, mame.fileroms, mame.filesl);
+				if (modified != 0)
+					file.setLastModified(modified);
+			}
+			catch (ParserConfigurationException | TransformerException e)
+			{
+				Log.err(e.getMessage(), e);
+			}
+		try (final var oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(ProfileNFO.getFileNfo(session, file)))))
 		{
 			oos.writeObject(this);
 		}
-		catch(final Throwable e)
+		catch (final Exception e)
 		{
-			Log.err(e.getMessage(),e);
+			Log.err(e.getMessage(), e);
 		}
 	}
 
 	/**
 	 * Does the attached file is a JRM file
+	 * 
 	 * @return true if it's a JRM file
 	 */
 	public boolean isJRM()
@@ -216,14 +234,18 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 
 	/**
 	 * Load JRM file and fill up {@link #mame}
-	 * @param jrmfile the JRM file to load
+	 * 
+	 * @param jrmfile
+	 *            the JRM file to load
 	 */
 	public void loadJrm(final File jrmfile)
 	{
-		final SAXParserFactory factory = SAXParserFactory.newInstance();
+		final var factory = SAXParserFactory.newInstance();
 		try
 		{
-			final SAXParser parser = factory.newSAXParser();
+			final var parser = factory.newSAXParser();
+			parser.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, ""); // Compliant
+			parser.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, ""); // compliant
 			parser.parse(jrmfile, new DefaultHandler()
 			{
 				private boolean in_jrm = false;
@@ -231,21 +253,23 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 				@Override
 				public void startElement(final String uri, final String localName, final String qName, final Attributes attributes) throws SAXException
 				{
-					if(qName.equalsIgnoreCase("JRomManager")) //$NON-NLS-1$
+					if (qName.equalsIgnoreCase("JRomManager")) //$NON-NLS-1$
 					{
 						in_jrm = true;
 					}
-					else if(qName.equalsIgnoreCase("Profile") && in_jrm) //$NON-NLS-1$
+					else if (qName.equalsIgnoreCase("Profile") && in_jrm) //$NON-NLS-1$
 					{
-						for(int i = 0; i < attributes.getLength(); i++)
+						for (var i = 0; i < attributes.getLength(); i++)
 						{
-							switch(attributes.getQName(i).toLowerCase())
+							switch (attributes.getQName(i).toLowerCase())
 							{
 								case "roms": //$NON-NLS-1$
 									mame.fileroms = new File(jrmfile.getParentFile(), attributes.getValue(i));
 									break;
 								case "sl": //$NON-NLS-1$
 									mame.filesl = new File(jrmfile.getParentFile(), attributes.getValue(i));
+									break;
+								default:
 									break;
 							}
 						}
@@ -255,56 +279,64 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 				@Override
 				public void endElement(final String uri, final String localName, final String qName) throws SAXException
 				{
-					if(qName.equalsIgnoreCase("JRomManager")) //$NON-NLS-1$
+					if (qName.equalsIgnoreCase("JRomManager")) //$NON-NLS-1$
 					{
 						in_jrm = false;
 					}
 				}
 			});
 		}
-		catch(ParserConfigurationException | SAXException | IOException e)
+		catch (ParserConfigurationException | SAXException | IOException e)
 		{
-//			JOptionPane.showMessageDialog(null, e, "Exception", JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
-			Log.err(e.getMessage(),e);
+			// JOptionPane.showMessageDialog(null, e, "Exception",
+			// JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
+			Log.err(e.getMessage(), e);
 		}
 	}
 
 	/**
 	 * save mame infos in JRM file
-	 * @param JrmFile the JRM file to save
-	 * @param roms_file the mame roms file
-	 * @param sl_file the software list file
+	 * 
+	 * @param jrmFile
+	 *            the JRM file to save
+	 * @param romsFile
+	 *            the mame roms file
+	 * @param slFile
+	 *            the software list file
 	 * @return return the {@code JrmFile}
 	 * @throws ParserConfigurationException
 	 * @throws TransformerException
 	 */
-	public static File saveJrm(final File JrmFile, final File roms_file, final File sl_file) throws ParserConfigurationException, TransformerException
+	public static File saveJrm(final File jrmFile, final File romsFile, final File slFile) throws ParserConfigurationException, TransformerException
 	{
-		final DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-		final DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-		final Document doc = docBuilder.newDocument();
-		final Element rootElement = doc.createElement("JRomManager"); //$NON-NLS-1$
+		final var docFactory = DocumentBuilderFactory.newInstance();
+		final var docBuilder = docFactory.newDocumentBuilder();
+		final var doc = docBuilder.newDocument();
+		final var rootElement = doc.createElement("JRomManager"); //$NON-NLS-1$
 		doc.appendChild(rootElement);
-		final Element profile = doc.createElement("Profile"); //$NON-NLS-1$
-		profile.setAttribute("roms", roms_file.getName()); //$NON-NLS-1$
-		if(sl_file != null)
-			profile.setAttribute("sl", sl_file.getName()); //$NON-NLS-1$
+		final var profile = doc.createElement("Profile"); //$NON-NLS-1$
+		profile.setAttribute("roms", romsFile.getName()); //$NON-NLS-1$
+		if (slFile != null)
+			profile.setAttribute("sl", slFile.getName()); //$NON-NLS-1$
 		rootElement.appendChild(profile);
-		final TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		final Transformer transformer = transformerFactory.newTransformer();
-		final DOMSource source = new DOMSource(doc);
-		final StreamResult result = new StreamResult(JrmFile);
+		final var transformerFactory = TransformerFactory.newInstance();
+		transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, ""); // Compliant
+		transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, ""); // Compliant
+		final var transformer = transformerFactory.newTransformer();
+		final var source = new DOMSource(doc);
+		final var result = new StreamResult(jrmFile);
 		transformer.transform(source, result);
-		return JrmFile;
+		return jrmFile;
 	}
-	
+
 	/**
-	 * Delete all related files to 
+	 * Delete all related files to
+	 * 
 	 * @return true on success
 	 */
 	public boolean delete()
 	{
-		if(file.delete())
+		if (file.delete())
 		{
 			mame.delete();
 			new File(file.getAbsolutePath() + ".cache").delete(); //$NON-NLS-1$
@@ -314,69 +346,62 @@ public final class ProfileNFO implements Serializable, HTMLRenderer
 		}
 		return false;
 	}
-	
+
 	public String getName()
 	{
 		return name;
 	}
-	
+
 	public String getVersion()
 	{
 		return toHTML(stats.version == null ? toGray("???") : toNoBR(stats.version)); //$NON-NLS-1$
 	}
-	
+
 	public String getHaveSets()
 	{
 		return toHTML(stats.haveSets == null ? (stats.totalSets == null ? toGray("?/?") : String.format("%s/%d", toGray("?"), stats.totalSets)) : String.format("%s/%d", stats.haveSets == 0 && stats.totalSets > 0 ? toRed("0") : (stats.haveSets.equals(stats.totalSets) ? toGreen(stats.haveSets + "") : toOrange(stats.haveSets + "")), stats.totalSets)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
 	}
-	
+
 	public String getHaveRoms()
 	{
 		return toHTML(stats.haveRoms == null ? (stats.totalRoms == null ? toGray("?/?") : String.format("%s/%d", toGray("?"), stats.totalRoms)) : String.format("%s/%d", stats.haveRoms == 0 && stats.totalRoms > 0 ? toRed("0") : (stats.haveRoms.equals(stats.totalRoms) ? toGreen(stats.haveRoms + "") : toOrange(stats.haveRoms + "")), stats.totalRoms)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
 	}
-	
+
 	public String getHaveDisks()
 	{
 		return toHTML(stats.haveDisks == null ? (stats.totalDisks == null ? toGray("?/?") : String.format("%s/%d", toGray("?"), stats.totalDisks)) : String.format("%s/%d", stats.haveDisks == 0 && stats.totalDisks > 0 ? toRed("0") : (stats.haveDisks.equals(stats.totalDisks) ? toGreen(stats.haveDisks + "") : toOrange(stats.haveDisks + "")), stats.totalDisks)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
 	}
-	
+
 	public String getCreated()
 	{
 		return toHTML(stats.created == null ? toGray("????-??-?? ??:??:??") : new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(stats.created)); //$NON-NLS-1$ //$NON-NLS-2$
 	}
-	
+
 	public String getScanned()
 	{
 		return toHTML(stats.scanned == null ? toGray("????-??-?? ??:??:??") : new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(stats.scanned)); //$NON-NLS-1$ //$NON-NLS-2$
 	}
-	
+
 	public String getFixed()
 	{
 		return toHTML(stats.fixed == null ? toGray("????-??-?? ??:??:??") : new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(stats.fixed)); //$NON-NLS-1$ //$NON-NLS-2$
 	}
-	
+
 	public static List<ProfileNFO> list(Session session, File dir)
 	{
 		List<ProfileNFO> rows = new ArrayList<>();
-		if(dir != null && dir.exists())
+		if (dir != null && dir.exists())
 		{
-			File filedir = dir;
-			if(filedir!=null)
+			final var filedir = dir;
+			if (filedir != null)
 			{
-				File[] files = filedir.listFiles((FilenameFilter) (dir1, name) -> {
-					final File f = new File(dir1, name);
-					if(f.isFile())
-						if(!Arrays.asList("cache", "properties", "nfo", "jrm1", "jrm2").contains(FilenameUtils.getExtension(name))) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
-							return true;
-					return false;
+				final File[] files = filedir.listFiles( (dir1, name) -> {
+					final var f = new File(dir1, name);
+					return (f.isFile() && !Arrays.asList("cache", "properties", "nfo", "jrm1", "jrm2").contains(FilenameUtils.getExtension(name))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 				});
-				if(files!=null)
+				if (files != null)
 				{
-					Arrays.asList(files).stream().map(f -> {
-						return ProfileNFO.load(session, f);
-					}).forEach(pnfo -> {
-						rows.add(pnfo);
-					});
+					Arrays.asList(files).stream().map(f -> ProfileNFO.load(session, f)).forEach(rows::add);
 				}
 			}
 		}
