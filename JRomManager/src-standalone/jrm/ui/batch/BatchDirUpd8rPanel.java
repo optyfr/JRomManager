f0d54831f58fc5b14baba1545ec29a9127d75e1e
optyfr <17027109+optyfr@users.noreply.github.com>
2021-08-15 08:52:56 +0200

    unused import

--- JRomManager/src-standalone/jrm/ui/batch/BatchDirUpd8rPanel.java
+++ JRomManager/src-standalone/jrm/ui/batch/BatchDirUpd8rPanel.java
@@ -5,8 +5,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
