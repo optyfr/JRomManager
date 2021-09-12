910e80ccb40879fa9bcd7704af2acb6b60e77aff
optyfr <17027109+optyfr@users.noreply.github.com>
2021-09-12 16:22:47 +0200

    Fix test packages that destroy eclipse project

--- JRomManager/test-cli/jrm/test/cli/CliTest.java
+++ JRomManager/test-cli/jrm/test/cli/CliTest.java
@@ -1,10 +1,5 @@
 package jrm.test.cli;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
-
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class CliTest	//NOSONAR
+class CliTest
 {
-	
 }
--- JRomManager/test/jrm/test/core/CoreTest.java
+++ JRomManager/test/jrm/test/core/CoreTest.java
@@ -1,10 +1,6 @@
 package jrm.test.core;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
-
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class CoreTest	//NOSONAR
+class CoreTest
 {
-	
+
 }
