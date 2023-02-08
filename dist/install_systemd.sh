6e1201b0b9e858a9349a54b1c9918a9d0c04a24e
optyfr <17027109+optyfr@users.noreply.github.com>
2023-02-08 15:28:47 +0100

    fix script

--- dist/install_systemd.sh
+++ dist/install_systemd.sh
@@ -11,8 +11,8 @@ Wants=network-online.target
 
 [Service]
 Type=forking
-ExecStart=$DIR/jsvc -cwd "$(DIR)" -home "$(JAVA_HOME)" -cp JRomManager.jar -pidfile /var/run/JRomManager.pid -procname JRomManager -outfile "$(DIR)/logs/JRomManager.log" -errfile "$(DIR)/logs/JRomManager.err" jrm.server.Server
-ExecStop=$DIR/jsvc -stop -cwd "$(DIR)" -home "$(JAVA_HOME)" -pidfile /var/run/JRomManager.pid -cp JRomManager.jar jrm.server.Server
+ExecStart="${DIR}/jsvc" -cwd "${DIR}" -home "${JAVA_HOME}" -cp JRomManager.jar -pidfile /var/run/JRomManager.pid -procname JRomManager -outfile "${DIR}/logs/JRomManager.log" -errfile "${DIR}/logs/JRomManager.err" jrm.server.Server
+ExecStop="${DIR}/jsvc" -stop -cwd "${DIR}" -home "${JAVA_HOME}" -pidfile /var/run/JRomManager.pid -cp JRomManager.jar jrm.server.Server
 TimeoutSec=1min
 
 [Install]
