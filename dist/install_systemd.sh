864f9f76175b083ce843e19b24be9a4052ab3c9c
optyfr <17027109+optyfr@users.noreply.github.com>
2023-02-09 09:58:18 +0100

    fix service link

--- dist/install_systemd.sh
+++ dist/install_systemd.sh
@@ -24,7 +24,7 @@ TimeoutSec=1min
 WantedBy=multi-user.target
 EOF
 
-sudo ln -sf JRomManager.service /etc/systemd/system/
+sudo ln -sf JRomManager.service /etc/systemd/system/JRomManager.service
 sudo systemctl enable JRomManager.service
 sudo systemctl start JRomManager.service
 
