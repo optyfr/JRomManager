92ab74dd6bb13fa9b54a50e96ea4f606773c6d94
Tevis Money <tpmoney@ncitguys.com>
2025-01-29 22:07:35 -0500

    Update classpath arguments to use `:` instead of `;`

--- dist/JRomManagerFullServer.sh
+++ dist/JRomManagerFullServer.sh
@@ -12,4 +12,4 @@ if [ $? -eq 0 ]; then
 		if [ $? -eq 0 ]; then OPT=-xmx2g; fi
 	fi
 fi
-java $OPT -cp "JRomManager.jar;lib/*"  jrm.fullserver.FullServer
+java $OPT -cp "JRomManager.jar:lib/*"  jrm.fullserver.FullServer
